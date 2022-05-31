8123a7e4aa58740107194d7a5973fddf85347013
talhaahmed09 <endhogaya3@gmail.com>
2022-05-31 16:56:00 +0500

    refactor: added data array instead of importing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
-import * as data from './data.json';
-import FilterableProductTable from './src/FilterableProductTable/FilterableProductTable';
+import FilterableProductTable from './components/FilterableProductTable/FilterableProductTable';
+
+const data =[
+  {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football", id:1},
+  {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball",id:2},
+  {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball",id:3},
+  {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch",id:4},
+  {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5",id:5},
+  {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7",id:6}
+];
+
+
 
 function App() {
   return (
     <div className="App">
-      <FilterableProductTable props={data}/>
+      <FilterableProductTable data={data}/>
     </div>
   );
 }
