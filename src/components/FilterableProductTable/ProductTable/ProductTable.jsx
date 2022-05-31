4ee6eff2762ea30523137b625f267e7be3c10757
talhaahmed09 <endhogaya3@gmail.com>
2022-05-31 16:50:27 +0500

    feat: table parent component

---
+++ src/components/FilterableProductTable/ProductTable/ProductTable.jsx
@@ -0,0 +1,56 @@
+import { Fragment, useEffect } from "react";
+import ProductCategoryRow from "./ProductCategoryRow/ProductCategoryRow";
+import ProductRow from "./ProductRow/ProductRow";
+
+const ProductTable = (props) => {
+  const { data, checked, filteredText } = props;
+
+  let filteredData = [];
+
+  let filteredResults = data.filter(
+    (item) => item.name.toLowerCase().indexOf(filteredText.toLowerCase()) > -1
+  );
+
+  if (checked) {
+    filteredResults = filteredResults.filter((item) => item.stocked == checked);
+  }
+
+  // setfilteredData(filteredResults);
+  filteredData = filteredResults;
+  let category = null;
+  console.log(filteredResults);
+
+  const updateCategory = (prev) => {
+    category = prev;
+  }
+
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {filteredData.map((item,i) => {
+            
+            return (
+              <Fragment key={i}>
+               
+              { item.category !== category && <ProductCategoryRow category={item.category} />}
+              {updateCategory(item.category) }
+                <ProductRow product={item}  />
+              </Fragment>
+            );
+            
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ProductTable;
--- src/src/FilterableProductTable/ProductTable/ProductTable.jsx
+++
@@ -1,9 +0,0 @@
-import ProductCategoryRow from "./ProductCategoryRow/ProductCategoryRow";
-import ProductRow from "./ProductRow/ProductRow";
-
-
-const ProductTable = () => {
-    return {}
-}
-
-export default ProductTable;
